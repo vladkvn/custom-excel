523bc81cfea659a265b8474bc3827d8ee3eb9af5
vladkvn <vlad.kvn@gmail.com>
2021-04-29 22:56:50 +0300

    Updated dev/prod behaviour

--- webpack.config.js
+++ webpack.config.js
@@ -4,47 +4,55 @@ const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const distFolder = path.resolve(__dirname, 'dist')
+module.exports = (env, argv) => {
 
-module.exports = {
-    context: path.resolve(__dirname, "src"),
-    entry: {
-        main: './index.js'
-    },
-    output: {
-        path: distFolder,
-        filename: '[name].bundle.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({template: './index.html'}),
-        new CopyPlugin({
-            patterns: [
-                {from: path.resolve(__dirname, "src/favicon.ico"), to: distFolder},
-            ]
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].bundle.css'
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
+    const isProd = argv.mode === 'production';
+    const isDev = !isProd;
+    const fileName = (postfix) => isDev ? `[name].bundle.${postfix}` : `[name].[contenthash].bundle.${postfix}`
+
+    return {
+        context: path.resolve(__dirname, "src"),
+        entry: {
+            main: './index.js'
+        },
+        output: {
+            path: distFolder,
+            filename: fileName('js'),
+            clean: true
+        },
+        devtool: isDev ? 'source-map' : false,
+        plugins: [
+            new HtmlWebpackPlugin({template: './index.html'}),
+            new CopyPlugin({
+                patterns: [
+                    {from: path.resolve(__dirname, "src/favicon.ico"), to: distFolder},
                 ]
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ['@babel/preset-env']
+            }),
+            new MiniCssExtractPlugin({
+                filename: fileName('css')
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                },
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
                     }
                 }
-            }
-        ]
+            ]
+        }
     }
-}
+};
